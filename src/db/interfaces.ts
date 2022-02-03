1388028164bce55c1a17c6dac8a084e2b2fd5309
rosen1000 <rosen.ruseff@gmail.com>
2022-02-03 13:04:44 +0200

    login/register

--- src/db/interfaces.ts
+++ src/db/interfaces.ts
@@ -1,19 +1,19 @@
 export interface User {
-    id: number,
-    username: string,
-    password: string
+    id: number;
+    username: string;
+    password: string;
 }
 
 export interface ToolRemote {
-    id: number,
-    json: string
+    id: number;
+    json: string;
 }
 
 export interface Tool {
-    type: string,
-    parts: Array<Part>
+    type: string;
+    parts: Array<Part>;
 }
 
 export interface Part {
-    material: string
+    material: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,63 @@
-import { Tool, User, ToolRemote } from './db/interfaces';
+import { User, ToolRemote } from './db/interfaces';
 import knex from './db/database';
+import { compareSync, hashSync } from 'bcrypt';
 import Fastify from 'fastify';
 const app = Fastify();
 
+const jwtOptions = {
+    secret: 'obicham_banica',
+    // requires fastify-cookie
+    // cookie: { cookieName: 'identifier' },
+};
+
 app.register(import('fastify-raw-body'), { field: 'rawBody' });
 app.register(import('fastify-multipart'), { addToBody: true });
+app.register(import('fastify-jwt'), { ...jwtOptions });
 app.register(import('fastify-formbody'));
+app.register(import('fastify-cookie'));
 app.register(import('fastify-cors'));
 
-app.get('/', (req, res) => {
-    res.send('ok');
+app.get('/', async (req, res) => {
+    let data = await req.jwtVerify();
+    res.send(data);
+});
+
+app.put('/register', async (req, _res) => {
+    const username = req.body['username'];
+    const password = req.body['password'];
+
+    if (
+        await knex<User>('users')
+            .select('*')
+            .where('username', username)
+            .first()
+    )
+        return { error: 'User already exists' };
+
+    knex<User>('users')
+        .insert({
+            username,
+            password: hashSync(password, 8),
+        })
+        .then((resp) => {})
+        .catch(console.log);
+
+    return app.jwt.sign({ sub: username, iat: Date.now() });
+});
+
+app.post('/login', async (req, _res) => {
+    let username = req.body['username'];
+    let password = req.body['password'];
+
+    let pick = await knex<User>('users')
+        .select('password')
+        .where('username', username)
+        .first();
+    if (pick == null) return { error: 'Wrong username or password' };
+    if (!compareSync(password, pick.password))
+        return { error: 'Wrong username or password' };
+
+    return app.jwt.sign({ sub: username, iat: Date.now() });
 });
 
 app.post('/tool', async (req, res) => {
@@ -21,18 +69,11 @@ app.post('/tool', async (req, res) => {
 });
 
 app.post('/new-tool', async (req, res) => {
-    await knex('tools').insert({json: JSON.stringify(req.body)});
+    await knex('tools').insert({ json: JSON.stringify(req.body) });
     res.send('ok');
 });
 
-app.listen(8080, (e) => {
+app.listen(8081, (e, address) => {
     if (e) return console.error(e);
-    console.log('Listening on http://localhost:8080');
+    console.log(`Listening on ${address}`);
 });
-
-// (async () => {
-//     // await knex('tools').insert({ json: JSON.stringify({ type: 'pickaxe', parts: [{material: "wood"}]}) });
-//     let pull = await knex<ToolRemote>('tools').first();
-//     let pull2 = JSON.parse(pull.json) as Tool;
-//     console.log(pull2);
-// })();
